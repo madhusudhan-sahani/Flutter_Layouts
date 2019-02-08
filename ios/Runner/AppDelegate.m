8dc7ef6f0f6d1778112c20fa533630d15a94e532
Narendra Maurya <nandymandy09@hotmail.com>
2019-02-08 21:18:24 +0530

    Simple Post page Created

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,14 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyCvvXyIzo1l8nr_5sLQwPjVGuqlECTj9oA"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
